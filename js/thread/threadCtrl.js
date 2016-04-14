d42cf2899848e562a62ab1352c3b2b68b8620bd8
ascotson <ascotson@gmail.com>
2016-04-14 14:44:02 -0600

    Changed the 2nd parameter in the  from '.thread' to 'thread' as  is implicit in the controller.

--- js/thread/threadCtrl.js
+++ js/thread/threadCtrl.js
@@ -2,7 +2,7 @@ angular.module('rtfmApp')
   .controller('threadCtrl', function($scope, $firebaseObject, $firebaseArray, threadRef, commentsRef){
 
     $scope.thread = $firebaseObject(threadRef);
-    $scope.thread.$bindTo($scope, '$scope.thread');
+    $scope.thread.$bindTo($scope, 'thread');
 
     $scope.comments = $firebaseArray(commentsRef);
 
